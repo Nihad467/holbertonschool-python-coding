ed9119010009b0a469c2abda4295b4eb6e0ad795
Nihad467 <nihadseferzade01@gmail.com>
2026-05-02 15:41:48 +0400

    fix: complete 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Module that defines a Square class."""
+"""This module defines a Square class."""
 
 
 class Square:
-    """A class that defines a square."""
+    """Represents a square."""
 
     def __init__(self, size):
+        """Initializes a square with a private size attribute."""
         self.__size = size
