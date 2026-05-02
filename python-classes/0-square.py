88cedae673f6f86bba2e9b67a5ece2086af3ae82
Nihad467 <nihadseferzade01@gmail.com>
2026-05-02 15:23:49 +0400

    0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""Module that defines a Square class."""
-
-
 class Square:
-    """A class that defines a square."""
-
     def __init__(self, size):
         self.__size = size
